173f19aad7c74c012219141df14d522a5f4b24cf
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-06-11 00:08:14 +0700

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import axios from "axios";
+import { TwitterApi } from 'twitter-api-v2';
+
+const twitterClient = new TwitterApi({
+  appKey: process.env.API_KEY,
+  appSecret: process.env.API_SECRET,
+  accessToken: process.env.AC_TOKEN,
+  accessSecret: process.env.AC_TOKEN_SC
+});
+
+setInterval(async() => {
+  let { data }  = await axios.get("https://watasalim.vercel.app/api/quotes/random");
+  let tweet = await twitterClient.v1.tweet(data.quote.body)
+  await twitterClient.v1.reply(data.quote.url, tweet.id_str)
+}, 3600000)
